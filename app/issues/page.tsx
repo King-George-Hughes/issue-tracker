b39eddf4e5d6a128ba222ceaa72b92a1ac8bacfb
King-George-Hughes <hughesgeorgeking@gmail.com>
2023-11-13 08:24:12 +0000

    added api call for all issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,6 @@
 import prisma from "@/prisma/client";
 import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
-import React from "react";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
